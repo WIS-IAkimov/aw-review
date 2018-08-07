0482212d5b105c1dbb310e04a73209afa6e1764a
Alexey <decent-alex@yandex.ru>
2018-08-07 15:52:07 +0300

    Add settings view
    Add settings view. Create functionality of statuses and columns

--- src/app/layout/widget-dashboard/components/widget-data-monitoring/widget.component.ts
+++ src/app/layout/widget-dashboard/components/widget-data-monitoring/widget.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnChanges, OnDestroy, Input } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
-import { IWidgetData, ICategoryTotal, IColumn } from './interfaces';
+import { IWidgetData, ICategoryTotal, IColumn, IStatus, ISettingsModel } from './interfaces';
 import { WidgetService } from './widget.service';
 
 @Component({
@@ -18,17 +18,42 @@ export class WidgetComponent implements OnInit, OnChanges, OnDestroy {
   subscriptionCategoryList: Subscription;
 
   data: Array<IWidgetData>;
-  columnList: Array<IColumn>;
+  // columnList: Array<IColumn>;
+  // availableStatuses: Array<IStatus>;
+  // filterByStatus: boolean;
+  // dials: object;
   categoryList: Array<ICategoryTotal>;
-  availableStatuses: Array<string>;
-  checkedStatuses: Array<string>;
   statusColors: object;
-  dials: object;
   activeDial: string;
+  settingsState: ISettingsModel;
+  editSettingsState: ISettingsModel;
 
   constructor(private widgetService: WidgetService, private modalService: NgbModal) {
-    this.availableStatuses = ['Pending', 'Assigned', 'Closed'];
-    this.checkedStatuses = ['Pending'];
+    this.settingsState = {
+      availableStatuses: [
+        {
+          name: 'Pending',
+          checked: true
+        },
+        {
+          name: 'Assigned',
+          checked: false
+        },
+        {
+          name: 'Closed',
+          checked: false
+        }
+      ],
+      filterByStatus: true,
+      dials: {
+        dial1: {Category: 'X1', TotalCount: 6},
+        dial2: {Category: 'X2', TotalCount: 6},
+        dial3: {Category: 'Other', TotalCount: 6}
+      },
+      columnList: []
+    };
+
+    this.editSettingsState = JSON.parse(JSON.stringify(this.settingsState));
 
     this.statusColors = {
       'Pending': '#ff0000',
@@ -37,12 +62,6 @@ export class WidgetComponent implements OnInit, OnChanges, OnDestroy {
     };
 
     this.activeDial = null;
-
-    this.dials = {
-      dial1: {Category: 'X1', TotalCount: 6},
-      dial2: {Category: 'X2', TotalCount: 6},
-      dial3: {Category: 'Other', TotalCount: 6}
-    };
   }
 
   ngOnInit() {
@@ -75,7 +94,8 @@ export class WidgetComponent implements OnInit, OnChanges, OnDestroy {
   normalizeData = (data) => {
     if (data !== undefined) {
       this.data = data;
-      this.columnList = Object.keys(data[0]).map((col, index) => ({name: col, checked: true}));
+      this.settingsState.columnList = Object.keys(data[0]).map((col, index) => ({name: col, checked: true}));
+      this.editSettingsState = JSON.parse(JSON.stringify(this.settingsState));
     } else {
       console.error('data from Observable is undefined');
     }
@@ -86,7 +106,7 @@ export class WidgetComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   saveSettings(close) {
-    console.log('Save!');
+    this.settingsState = JSON.parse(JSON.stringify(this.editSettingsState));
     close();
   }
 
@@ -95,8 +115,37 @@ export class WidgetComponent implements OnInit, OnChanges, OnDestroy {
       this.activeDial = null;
     } else {
       this.activeDial = dial;
-      console.log(dial, 'choose category');
     }
+  }
+
+  private onFilterByStatus(event) {
+    const isAllStatusesFalse = !this.editSettingsState.availableStatuses.filter((status) => status.checked).length;
+
+    if (event && isAllStatusesFalse) {
+      this.editSettingsState.availableStatuses[0].checked = true;
+    }
+  }
+
+  private changeStatusFilter(event, statusName) {
+    if (this.editSettingsState.filterByStatus) {
+      const currentStatus = this.editSettingsState.availableStatuses.find((status) => status.name === statusName);
+      const isFalseState = currentStatus.checked;
+      const isAllStatusesFalse = !(this.editSettingsState.availableStatuses.filter((status) => status.checked).length > 1);
+
+      if (isFalseState && isAllStatusesFalse) {
+        event.preventDefault();
+        event.stopPropagation();
+        currentStatus.checked = true;
+
+        return;
+      }
+    }
+  }
+
+  private toggleColumnState(colName) {
+    const column = this.editSettingsState.columnList.find((col) => col.name === colName);
+
+    column.checked = !column.checked;
   }
 
   private getStatusColor(status) {
@@ -104,16 +153,16 @@ export class WidgetComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   private get _columns(): Array<string> {
-    return this.columnList.filter((col) => col.checked).map((col) => col.name);
+    return this.settingsState.columnList.filter((col) => col.checked).map((col) => col.name);
   }
 
   private get _data() {
     const newData = this.data
       .filter((data) => {
-        const activeCategory = this.dials[this.activeDial].Category;
+        const activeCategory = this.settingsState.dials[this.activeDial].Category;
 
         if (activeCategory === 'Other') {
-          const {[this.activeDial]: currentDial, ...otherDials} = this.dials;
+          const {[this.activeDial]: currentDial, ...otherDials} = this.settingsState.dials;
           const otherCategory = Object.keys(otherDials).map((key) => otherDials[key].Category);
 
           if (!otherCategory.includes(data.Category)) {
@@ -122,10 +171,18 @@ export class WidgetComponent implements OnInit, OnChanges, OnDestroy {
             return false;
           }
         } else {
-          return data.Category === this.dials[this.activeDial].Category;
+          return data.Category === this.settingsState.dials[this.activeDial].Category;
         }
       })
-      .filter((data) => this.checkedStatuses.includes(data.Status));
+      .filter((data) => {
+        if (!this.settingsState.filterByStatus) {
+          return true;
+        }
+
+        const statusIsActive = this.settingsState.availableStatuses.find((status) => status.name === data.Status && status.checked);
+
+        return statusIsActive;
+      });
 
     return newData;
   }
